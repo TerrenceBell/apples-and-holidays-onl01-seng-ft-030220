e5eb84660296bb8bbbfbe8772a3a58ccc48359e6
Terrence Bell <terrencekbell@gmail.com>
2020-03-10 14:38:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
-      supplies = supplies.join(",")
+      supplies = supplies.join(", ")
        holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
        puts "  " + holiday + " " + supplies
   end    
